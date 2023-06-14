6b3b99d99d03e6d18e03905d535920d991adfef3
Marina Portugal <marinapsvreis@gmail.com>
2023-06-14 19:06:14 -0300

    :sparkles: Form in Register page

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -8,7 +8,7 @@ import { Form, FormAnnotation } from './styles'
 const claimUsernameFormSchema = z.object({
   username: z
     .string()
-    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
     .regex(/^([a-z\\-]+)$/i, {
       message: 'O usuário pode ter apenas letras e hifens.',
     })
