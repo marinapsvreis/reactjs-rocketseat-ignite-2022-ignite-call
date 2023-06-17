d25471d919e07809a1de8a5d89bef2ef1b8ad067
Marina Portugal <marinapsvreis@gmail.com>
2023-06-17 14:57:49 -0300

    :lipstick: Time Intervals Page style and structure

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -5,7 +5,7 @@ import { ArrowRight, Check } from 'phosphor-react'
 import { Container, Header } from '../styles'
 import { AuthError, ConnectBox, ConnectItem } from './styles'
 
-export default function Register() {
+export default function ConectCalendar() {
   const session = useSession()
   const router = useRouter()
 
