cd3978f311174d0a3e1a354bd46f8b26288c4887
Marina Portugal <marinapsvreis@gmail.com>
2023-06-25 17:58:03 -0300

    :construction: Calendar and Timepicker initial style

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -44,10 +44,33 @@ export function Calendar() {
               <CalendarDay>1</CalendarDay>
             </td>
             <td>
-              <CalendarDay>2</CalendarDay>
+              <CalendarDay disabled>2</CalendarDay>
             </td>
             <td>
               <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <CalendarDay>4</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>5</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>6</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>7</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>8</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>9</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>10</CalendarDay>
             </td>
           </tr>
         </tbody>
