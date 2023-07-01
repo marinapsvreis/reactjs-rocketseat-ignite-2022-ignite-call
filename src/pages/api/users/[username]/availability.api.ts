636344cf4600295860f610607b5450c6d1e953a5
Marina Portugal <marinapsvreis@gmail.com>
2023-07-01 15:30:27 -0300

    :bug: Error by timezone +3 hours

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -79,9 +79,7 @@ export default async function handle(
     const isTimeBlocked = blockedTimes.some(
       (blockedTime) => blockedTime.date.getHours() === time,
     )
-
     const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date())
-
     return !isTimeBlocked && !isTimeInPast
   })
 
